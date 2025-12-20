20ad982cf2c4861afac6f5a04cd5797d03f37c0f
Sudhanshu Raj <sudhanshu.ok1802@gmail.com>
2025-12-20 22:00:24 +0530

    New changes in Footer.jsx

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,17 +1,15 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import logo from '../assets/logo.png'
 function Footer() {
   return (
     <footer className='text-center bg-neutral-600 bg-opacity-35 text-neutral-400 py-1 my-10'>
       <div className=' flex justify-center items-center top-8'>
-        <img src={logo} alt="logo" width={100} className='mix-blend-lighten ' />
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQBssfGLNtRm8MsqZwnUBBxKbJ03p75nsLlkQ&s" alt="logo" width={200} className='mix-blend-lighten ' />
       </div>
       <div className='flex items-center justify-center gap-4 text-xl'>
         <Link to="/">About</Link>
         <Link to="/">Contact</Link>
       </div>
-      <p className='text-xl'>Created By Raju Bhai</p>
     </footer>
   )
 }
